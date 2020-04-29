6a9e50c6bd497b900145c7038fd174a3478933e8
Thomas Villain <atelier@anybodesign.com>
2020-04-29 11:19:08 +0200

    Footer copyright CSS

--- footer.php
+++ footer.php
@@ -16,40 +16,62 @@
 
 		<footer role="contentinfo" id="site_foot">
 			
-			<div class="row inner">
-				<div class="footer-content">				
+			<div class="inner">
+				
+				<div class="footer-content<?php if (get_theme_mod('author')) { echo ' has-author'; } ?>">				
 					
-					<?php // The credit/copyright line, settings in the Customizer ?>
-					
-					<p class="footer-copyright">
-						<?php if(get_theme_mod('footer_text')) {
-							echo get_theme_mod('footer_text', ''); 
-						} else {
-							echo '&copy;'; echo date(' Y '); echo esc_url(bloginfo('name')).'.'; 	
+					<?php if (get_theme_mod('author') || has_nav_menu( 'social_menu' )) { ?> 
+					<div class="footer-section">
+						<?php if (get_theme_mod('author')) {
+							get_template_part('template-parts/footer', 'author'); 
 						} ?>
-						
-						<a class="wp-love<?php if ( get_theme_mod('display_wp' ) == false ) { echo ' out-of-reach'; } ?>" href="//wordpress.org"><?php _e('Powered by WordPress!', 'good-time'); ?></a>
-					</p>
-					
+
+						<?php if ( has_nav_menu( 'social_menu' ) ) : ?>
+						<nav class="footer-nav" role="navigation" aria-label="<?php _e('Social Networks', 'fs-notes'); ?>">
+						<?php wp_nav_menu( array(
+								'theme_location'	=> 	'social_menu',
+								'menu_class'		=>	'social-menu',
+								'container'			=>	false
+								));
+						?>
+						</nav>
+						<?php endif; ?>
+					</div>
+					<?php } ?>
+
 					<?php // The footer menu location ?>
 					
 					<?php if ( has_nav_menu( 'footer_menu' ) ) : ?>
-					<nav class="footer-nav" role="navigation" aria-label="<?php _e('Footer Menu', 'fs-notes'); ?>">
-					<?php wp_nav_menu( array(
-							'theme_location'	=> 	'footer_menu',
-							'menu_class'		=>	'footer-menu',
-							'container'			=>	false
-							));
-					?>
-					</nav>
+					<div class="footer-section">
+						<nav class="footer-nav" role="navigation" aria-label="<?php _e('Footer Menu', 'fs-notes'); ?>">
+						<?php wp_nav_menu( array(
+								'theme_location'	=> 	'footer_menu',
+								'menu_class'		=>	'footer-menu',
+								'container'			=>	false
+								));
+						?>
+						</nav>
+					</div>
 					<?php endif; ?>
-					
 				</div>
+
+				<?php // The credit/copyright line, settings in the Customizer ?>
+				
+				<p class="footer-copyright">
+					<?php if(get_theme_mod('footer_text')) {
+						echo get_theme_mod('footer_text', ''); 
+					} else {
+						echo '&copy;'; echo date(' Y '); echo esc_url(bloginfo('name')).'.'; 	
+					} ?>
+					
+					<a class="wp-love<?php if ( get_theme_mod('display_wp' ) == false ) { echo ' out-of-reach'; } ?>" href="//wordpress.org"><?php _e('Powered by WordPress!', 'good-time'); ?></a>
+				</p>
+
 			</div>
 			
 		</footer>
 
-		<?php if(get_theme_mod('back2top') == true) { ?>
+		<?php if ( get_theme_mod('back2top') == true ) { ?>
 			<button id="back2top" title="<?php _e('Back to top','fs-notes'); ?>">
 				<img src="<?php bloginfo( 'template_directory' ); ?>/img/ui/back-to-top.svg" alt="">
 			</button>
