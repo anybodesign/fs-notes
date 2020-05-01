1d85469982f1f5ba1aad0e3b104952280e17c6bd
Thomas Villain <atelier@anybodesign.com>
2020-05-01 15:36:11 +0200

    Titles for archives

--- index.php
+++ index.php
@@ -13,6 +13,16 @@ get_header(); ?>
 
 				<div class="page-content">
 
+					<?php if ( is_home() && ! is_front_page() ) { ?>
+						<h1 class="page-title"><?php single_post_title(); ?></h1>
+					<?php } else if ( is_archive() ) { ?>
+						<h1 class="page-title"><?php the_archive_title(); ?></h1>
+					<?php } else if ( is_search() ) { ?>
+						<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'fs-notes' ), '<span class="search-term">' . get_search_query() . '</span>' ); ?></h1>
+					<?php } else { ?> 
+						<?php // No title ^^ ?>
+					<?php } ?>
+
 					<?php // The Loop
 					if ( have_posts() ) :		
 						while ( have_posts() ) : the_post();
