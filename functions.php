a689f67b2924e5cfc75861484d433f7b45874a2d
Thomas Villain <atelier@anybodesign.com>
2020-05-06 08:53:45 +0200

    Removed placeholder

--- functions.php
+++ functions.php
@@ -464,7 +464,7 @@ add_filter('tiny_mce_before_init', 'fs_tiny_formats');
 function fs_search_form( $form ) {
     $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
     <label class="screen-reader-text" for="s">' . __( 'Search for:' ) . '</label>
-    <input type="search" placeholder="' . __( 'Keywords' ) . '" value="' . get_search_query() . '" name="s" id="s">
+    <input type="search" value="' . get_search_query() . '" name="s" id="s">
     <input type="submit" class="action-btn" id="searchsubmit" value="'. esc_attr__( 'Search' ) .'">
     </form>';
  
